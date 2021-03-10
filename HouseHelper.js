ae50ac5dc29422ed92086dfc125b5f8857387691
ChernousovKirill <73911832+ChernousovKirill@users.noreply.github.com>
2021-03-10 20:12:35 +0200

    Update HouseHelper.js

--- HouseHelper.js
+++ HouseHelper.js
@@ -1,9 +1,19 @@
-({      
-    House: function(component) {
-        var action = component.get('c.getHouses');
-        var self = this;
-        action.setCallback(this, function(actionResult) {
-            component.set('v.houses', actionResult.getReturnValue());
+({
+    fetchHouseHelper : function(component, event, helper) {
+        component.set('v.mycolumns', [
+                {label: 'Name', fieldName: 'Name', type: 'Text'},
+                {label: 'Adress', fieldName: 'Adress__c', type: 'Text'},
+                {label: 'Price', fieldName: 'Priece__c', type: 'Currency'},
+                {label: 'Child', fieldName: 'Number_Child__c', type: 'Integer '}
+            ]);
+        var action = component.get("c.fetchHouses");
+        action.setParams({
+        });
+        action.setCallback(this, function(response){
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.houseList", response.getReturnValue());
+            }
         });
         $A.enqueueAction(action);
     }
