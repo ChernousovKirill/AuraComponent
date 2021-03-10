97e5b4cc22a6094025858fdb299386e757498fec
ChernousovKirill <73911832+ChernousovKirill@users.noreply.github.com>
2021-03-10 20:12:09 +0200

    Update HouseController.js

--- HouseController.js
+++ HouseController.js
@@ -1,19 +1,7 @@
-({	    doInit: function(component, event, helper) {
-        helper.House(component);
-    },
-    searchKeyChange: function(component, event) {
-        var searchKey = component.find("searchKey").get("v.value");
-        console.log('searchKey'+searchKey);
-        var action = component.get("c.findByName");
-        action.setParams({
-            "searchKey": searchKey
-        });
-        action.setCallback(this, function(a) {
-            component.set("v.houses", a.getReturnValue());
-        });
-        $A.enqueueAction(action);
-    },
-
+({
+    fetchHouse : function(component, event, helper) {
+        helper.fetchHouseHelper(component, event, helper);
+    }, 
  createNewHouse : function(component, event, helper) {
         var recordEvent=$A.get("e.force:createRecord");
         recordEvent.setParams({
