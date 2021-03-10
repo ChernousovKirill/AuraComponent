8bcf689c32ddc117b390c2944cd4a50f97422ed0
ChernousovKirill <73911832+ChernousovKirill@users.noreply.github.com>
2021-03-10 20:13:00 +0200

    Update HouseHelper.js

--- HouseHelper.js
+++ HouseHelper.js
@@ -1,20 +1,7 @@
-({
-    fetchHouseHelper : function(component, event, helper) {
-        component.set('v.mycolumns', [
-                {label: 'Name', fieldName: 'Name', type: 'Text'},
-                {label: 'Adress', fieldName: 'Adress__c', type: 'Text'},
-                {label: 'Price', fieldName: 'Priece__c', type: 'Currency'},
-                {label: 'Child', fieldName: 'Number_Child__c', type: 'Integer '}
-            ]);
-        var action = component.get("c.fetchHouses");
-        action.setParams({
-        });
-        action.setCallback(this, function(response){
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.houseList", response.getReturnValue());
-            }
-        });
-        $A.enqueueAction(action);
-    }
-})
+public class HouseController{
+  @AuraEnabled
+  public static List <House__c> fetchHouses() {
+      List<House__c> houseList = [SELECT Id, Name, Adress__c, Priece__c, Number_Child__c FROM House__c];
+      return houseList;
+  }
+}
